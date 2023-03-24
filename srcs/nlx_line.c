372223984ab8ba91ba8c901b78c7a4210bf841cb
gd-harco <gd-harco@student.42lyon.fr>
2023-03-24 19:49:00 +0100

    ne fonctionne pas pour ligne verticale, fonctionne pour le reste

--- srcs/nlx_line.c
+++ srcs/nlx_line.c
@@ -14,79 +14,88 @@
 #include "nlx_img.h"
 #include "nlx_color.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
-t_nlx_line	*create_line(t_vec3D p1, t_vec3D p2)
+bool	is_pixel_in_img(int x, int y, int height, int width)
 {
-	t_nlx_line	*new_line;
+	if (x >= 0 && x < width)
+	{
+		if (y >= 0 && y < height)
+			return (true);
+	}
+	return (false);
+}
 
-	new_line = malloc(sizeof(t_nlx_line));
+t_nlx_line	create_line(t_vec3D p1, t_vec3D p2)
+{
+	t_nlx_line	new_line;
+
 	if (p1.x < p2.x)
 	{
-		new_line->start = p1;
-		new_line->end = p2;
+		new_line.start = p1;
+		new_line.end = p2;
+	}
+	else if (p1.x > p2.x)
+	{
+		new_line.start = p2;
+		new_line.end = p1;
+	}
+	else
+		return (create_straight_line(p1, p2));
+	new_line.delta_x = abs(new_line.end.x - new_line.start.x);
+	new_line.delta_y = abs(new_line.end.y - new_line.start.y);
+	new_line.var_y = 0;
+	if (new_line.end.y < new_line.start.y)
+		new_line.var_y = -1;
+	else if (new_line.end.y > new_line.start.y)
+		new_line.var_y = 1;
+	new_line.var_x = 1;
+	return (new_line);
+}
+
+t_nlx_line	create_straight_line(t_vec3D p1, t_vec3D p2)
+{
+	t_nlx_line	straight;
+
+	if (p1.y < p2.y)
+	{
+		straight.start = p1;
+		straight.end = p2;
 	}
 	else
 	{
-		new_line->start = p2;
-		new_line->end = p1;
+		straight.start = p2;
+		straight.end = p1;
 	}
-	new_line->delta_x = new_line->end.x - new_line->start.x;
-	new_line->delta_y = new_line->end.y - new_line->start.y;
-	return (new_line);
+
+	straight.delta_x = 0;
+	straight.delta_y = abs(straight.end.y - straight.start.y);
+	straight.var_y = 1;
+	straight.var_x = 0;
+	return (straight);
 }
 
 void	nlx_draw_line(t_img *img, t_nlx_line *to_draw, int color)
 {
-	if (to_draw->delta_y > 0)
-		nlx_draw_downward(img, to_draw, color);
-	else
-		nlx_draw_upward(img, to_draw, color);
-}
-
-void	nlx_draw_downward(t_img *img, t_nlx_line *to_draw, int color)
-{
 	int		x;
 	int		y;
+	float	m;
+	float	e;
 
+	m = to_draw->delta_y / to_draw->delta_x;
 	x = to_draw->start.x;
 	y = to_draw->start.y;
-	to_draw->d = 2 * to_draw->delta_y - to_draw->delta_x;
-	while (x < to_draw->end.x)
+	e = 0;
+	while (x <= to_draw->end.x)
 	{
-		if ((x >= 0 && y >= 0)
-			&& (x < img->height && y < img->width))
-			nlx_pixel_put(img, x, y, COLOR_BLUE);
-		if (to_draw->d < 0)
-			to_draw->d += 2 * to_draw->delta_y;
-		else
-		{
-			to_draw->d += 2 * (to_draw->delta_y - to_draw->delta_x);
-			y++;
-		}
-	x++;
-	}
-}
-
-void	nlx_draw_upward(t_img *img, t_nlx_line *to_draw, int color)
-{
-	int		x;
-	int		y;
-
-	x = to_draw->start.x;
-	y = to_draw->start.y;
-	to_draw->d = 2 * to_draw->delta_y - to_draw->delta_x;
-	while (x < to_draw->end.x)
-	{
-		if ((x >= 0 && y >= 0)
-			&& (x < img->height && y < img->width))
-			nlx_pixel_put(img, x, y, COLOR_BLUE);
-		if (to_draw->d < 0)
-		{
-			to_draw->d += 2 * to_draw->delta_y;
-			y--;
-		}
-		else
-			to_draw->d += 2 * (to_draw->delta_y - to_draw->delta_x);
+		if (is_pixel_in_img(x, y, img->height, img->width))
+			nlx_pixel_put(img, x, y, color);
+		e -= m;
 		x++;
+		if (e < -0.5)
+		{
+			y += to_draw->var_y;
+			e += 1.0;
+		}
 	}
 }
--- test.c
+++ test.c
@@ -11,24 +11,16 @@ void main(void)
 	t_img	img;
 	t_vec3D	p1;
 	t_vec3D	p2;
-	t_vec3D p3;
-	t_vec3D p4;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 1280, 720, "TEST");
 	nlx_new_image(&img, mlx, 1280, 720);
-	p1.x = 0;
+	p1.x = 640;
 	p1.y = 0;
-	p2.x = 1279;
+	p2.x = 640;
 	p2.y = 719;
-	p3.x = 0;
-	p3.y = 719;
-	p4.x = 1279;
-	p4.y = 0;
-	t_nlx_line *line = create_line(p1, p2);
-	t_nlx_line *line2 = create_line(p3, p4);
-	nlx_draw_line(&img, line, COLOR_BLUE);
-	nlx_draw_line(&img, line2, COLOR_DARK_GRASS);
+	t_nlx_line line = create_line(p1, p2);
+	nlx_draw_line(&img, &line, COLOR_BLUE);
 	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
