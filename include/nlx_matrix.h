243458e2339e53467e9f4c7fc81b8400e6504a6b
gd-harco <gd-harco@student.42lyon.fr>
2023-03-27 12:34:37 +0200

    made some change to matrix.c/h

--- include/nlx_matrix.h
+++ include/nlx_matrix.h
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 16:15:08 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/03/26 16:18:24 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2023/03/26 19:52:32 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 # define NLX_MATRIX_H
 
 # include "nlx_vec.h"
-# include "libft.h"
 
 typedef struct s_matrix
 {
 	double	m[4][4];
 }				t_matrix;
 
-void	init_matrix(t_matrix *m);
+typedef struct s_proj_m
+{
+	t_matrix	m;
+	double		z_near;
+	double		z_far;
+	double		fov;
+	double		aspect_ratio;
+}	t_proj_m;
+
+void		create_identity_matrix(t_matrix *m);
+t_matrix	get_projection_matrix(t_proj_m *data);
 
 #endif
--- srcs/nlx_matrix.c
+++ srcs/nlx_matrix.c
@@ -6,13 +6,43 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 16:17:30 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/03/26 16:24:49 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2023/03/26 19:52:27 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "nlx_matrix.h"
 
-void	init_matrix(t_matrix *matrix)
+void	create_identity_matrix(t_matrix *m)
 {
-	ft_bzero(matrix, sizeof(t_matrix));
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (i == j)
+				m->m[i][j] = 1;
+			else
+				m->m[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+}
+
+t_matrix	get_projection_matrix(t_proj_m *data)
+{
+	t_matrix	proj_matrix;
+
+	create_identity_matrix(&proj_matrix);
+	proj_matrix.m[0][0] = data->aspect_ratio * (1 / (tan(data->fov / 2)));
+	proj_matrix.m[1][1] = 1 / (tan(data->fov / 2));
+	proj_matrix.m[2][2] = data->z_far / (data->z_far - data->z_near);
+	proj_matrix.m[3][2] = (-data->z_far * data->z_near)
+		/ (data->z_far - data->z_near);
+	proj_matrix.m[2][3] = 1;
+	return (proj_matrix);
 }
